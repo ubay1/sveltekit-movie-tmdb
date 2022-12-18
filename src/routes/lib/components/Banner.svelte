3a892ae57f6b07d5539a4df256dbd7e96c823a10
ubay182 <ubay00804@gmail.com>
2022-12-19 00:04:21 +0700

    banner fix

--- src/routes/lib/components/Banner.svelte
+++ src/routes/lib/components/Banner.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
 	import { PUBLIC_BACKDROP_IMAGE_URL } from '$env/static/public';
-	import { FormatNumber } from 'lib/helpers/format';
+	import { FormatNumber, toHoursAndMinutes } from 'lib/helpers/format';
 	import type { TypeBannerMovie } from 'lib/types/movie';
 	import StarRating from './StarRating.svelte';
 
 	export let content: TypeBannerMovie[] = [];
-
-	console.log(content);
 </script>
 
-<div class="w-full h-90 bg-gray-4 flex">
+<div class="w-full h-90 bg-gray-7">
 	{#each content as item, index}
 		<div class="relative bg-black aspect-ratio-1/1 md:aspect-ratio-3/2 lg:aspect-ratio-25/9">
 			<div class="absolute top-0 right-0 lt-lg:left-0 lg:bottom-0 lg:left-1/3 bg-gray-6">
@@ -18,7 +16,7 @@
 					alt="img-banner"
 					width="400"
 					height="250"
-					class="w-full h-full object-cover bg-gray-5"
+					class="w-full h-full object-cover bg-gray-7"
 				/>
 			</div>
 			<div
@@ -26,12 +24,16 @@
 				via-black to-transparent lt-lg:bg-gradient-to-t lt-lg:right-0 lt-lg:p-10 lg:pl-25 lg:w-2/4 lg:bg-gradient-to-r"
 			>
 				<div class="h-full flex justify-center flex-col">
-					<div class="text-5xl font-semibold mb-4">{item.title}</div>
-					<div class="flex gap-3">
+					<div class="text-5xl font-semibold">{item.title}</div>
+					<div class="flex gap-3 mt-4 mb-3">
 						<StarRating rating={item.vote_average} />
-						<div class="text-gray-4">{item.vote_average}</div>
-						<div class="text-gray-4">{FormatNumber(item.vote_count)} Reviews</div>
-						<div class="text-gray-4">{new Date(item.release_date).getFullYear()}</div>
+						<div class="text-gray-5">{item.vote_average}</div>
+						<div class="text-gray-5">{FormatNumber(item.vote_count)} Reviews</div>
+						<div class="text-gray-5">{new Date(item.release_date).getFullYear()}</div>
+						<div class="text-gray-5">{toHoursAndMinutes(item.runtime)}</div>
+					</div>
+					<div class="leading-6 pr-4 text-gray-3">
+						{item.overview}
 					</div>
 				</div>
 			</div>
--- src/routes/lib/helpers/format.ts
+++ src/routes/lib/helpers/format.ts
@@ -1,3 +1,10 @@
 export const FormatNumber = (value: number): number => {
 	return Number(new Intl.NumberFormat('id-ID').format(value));
 };
+
+export const toHoursAndMinutes = (totalMinutes: number) => {
+	const hours = Math.floor(totalMinutes / 60);
+	const minutes = totalMinutes % 60;
+
+	return `${hours}h ${minutes}min`;
+};
