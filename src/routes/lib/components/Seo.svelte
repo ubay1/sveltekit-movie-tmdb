916cdc73ad859d5005f4b19cfe5b6f47c0964f2b
ubay182 <ubay00804@gmail.com>
2023-01-31 11:13:07 +0700

    fix meta image

--- src/routes/lib/components/Seo.svelte
+++ src/routes/lib/components/Seo.svelte
@@ -29,5 +29,8 @@
 	<meta property="og:title" content={title} />
 	<meta property="og:url" content={url} />
 	<meta property="og:description" content={metadescription} />
-	<meta property="og:image" content={image.url} />
+	<meta property="og:image:secure" content={image.url} />
+	<meta property="og:image:width" content="800" />
+	<meta property="og:image:height" content="450" />
+	<meta property="og:image:type" content="image/png" />
 </svelte:head>
